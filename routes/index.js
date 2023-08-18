d7101d5dda8ad020d6555a437de6075c1592daba
Ahmed ALHalwagy <ahmedhalwgy@gmail.com>
2023-08-18 12:56:56 +0300

    Refactor main routes in app.js

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const categoryRoutes = require('./categoryRoutes');
+const subCatRoutes = require('./subCatRoutes');
+const brandRoutes = require('./brandRoutes');
+const productRoutes = require('./productRoutes');
+const userRoutes = require('./userRoutes');
+const reviewRoutes = require('./reviewRoutes');
+const wishListRoutes = require('./wishListRoutes');
+const addressRoutes = require('./addressRoutes');
+const couponRoutes = require('./couponRoutes');
+
+const mountRoutes = (app) => {
+  app.use('/api/v1/category', categoryRoutes);
+  app.use('/api/v1/subcategory', subCatRoutes);
+  app.use('/api/v1/brand', brandRoutes);
+  app.use('/api/v1/product', productRoutes);
+  app.use('/api/v1/user', userRoutes);
+  app.use('/api/v1/review', reviewRoutes);
+  app.use('/api/v1/wishlist', wishListRoutes);
+  app.use('/api/v1/address', addressRoutes);
+  app.use('/api/v1/coupon', couponRoutes);
+};
+
+module.exports = mountRoutes;
